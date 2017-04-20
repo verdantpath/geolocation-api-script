416ad19eb0c855ad8341df5fac5a2e1c00d4f021
Matt <m_dittmann@yahoo.com>
2017-04-19 21:48:03 -0700

    check if geolocation is supported and ask for location

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,6 +1,7 @@
 var elMap = document.getElementById('loc');
 var msg = 'Sorry, we were unable to get your location.';
 
+// CHECK IF GEOLOCATION IS SUPPORTED AND ASK FOR LOCATION
 if (Modernizr.geolocation) {
 	navigator.geolocation.getCurrentPostition(success, fail);
 	elMap.textContent = 'Checking location...';
