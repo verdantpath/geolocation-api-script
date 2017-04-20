a4de1dc60e2dffd01aca489b861cd26e447afe15
Matt <m_dittmann@yahoo.com>
2017-04-19 22:03:14 -0700

    fixed typo in getCurrentPosition() method

--- js/geolocation.js
+++ js/geolocation.js
@@ -3,7 +3,7 @@ var msg = 'Sorry, we were unable to get your location.';
 
 // CHECK IF GEOLOCATION IS SUPPORTED AND ASK FOR LOCATION
 if (Modernizr.geolocation) {
-	navigator.geolocation.getCurrentPostition(success, fail);
+	navigator.geolocation.getCurrentPosition(success, fail);
 	elMap.textContent = 'Checking location...';
 } else {
 	elMap.textContent = msg;
